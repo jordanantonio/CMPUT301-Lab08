dda2f7d82e3273aefffbac64623b9b9f332b7a46
jordanantonio <jordanantonio727@gmail.com>
2026-03-06 12:33:58 -0700

    Delete city test

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -13,4 +13,13 @@ public class CustomListTest {
         assertTrue(list.hasCity(calgary));
     }
 
+    @Test
+    public void testDeleteCity() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        list.addCity(calgary);
+
+        assertFalse(list.deleteCity(calgary));
+    }
+
 }
